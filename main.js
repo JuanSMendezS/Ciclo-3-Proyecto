c40d3e15438449cc121a533e6e60d199ad770bfd
Juan Mendez <jmendez16@cuc.edu.co>
2022-09-21 10:03:13 -0500

    Primer commit

---
+++ main.js
@@ -0,0 +1,16 @@
+function pantallapiloto(){
+    let titulopiloto = $
+    alert("piloto")
+}
+
+function pantallausuario(){
+    alert("usuario")
+}
+
+function pantallaadmin(){
+    alert("admin")
+}
+
+function iniciarsesion(){
+    alert("Bienvenido")
+}
